6d033e31701c299ef68a1586a45ade62024cca12
Bluem <140798194+JohnCarloSabenorio@users.noreply.github.com>
2026-01-31 23:36:32 +0800

    fixed social media icons on footer

--- src/components/SocialMediaIcons/SocialMediaIcons.tsx
+++ src/components/SocialMediaIcons/SocialMediaIcons.tsx
@@ -3,8 +3,8 @@ import LinkedInIcon from "@mui/icons-material/LinkedIn";
 import FacebookIcon from "@mui/icons-material/Facebook";
 export default function SocialMediaIcons() {
   return (
-    <div className="flex justify-center md:justify-start gap-1">
-      <a href="https://github.com/jc-sabenorio" target="_blank">
+    <div className="flex justify-center md:justify-start gap-1 lg:hidden">
+      <a rel="noopener" href="https://github.com/jc-sabenorio" target="_blank">
         <GitHubIcon
           sx={{
             fontSize: 30,
@@ -16,6 +16,7 @@ export default function SocialMediaIcons() {
         />
       </a>
       <a
+        rel="noopener"
         href="https://www.linkedin.com/in/john-carlo-sabenorio-732259244/"
         target="_blank"
       >
@@ -29,7 +30,11 @@ export default function SocialMediaIcons() {
           }}
         />
       </a>
-      <a href="https://www.facebook.com/jc.sabenorio.2025" target="_blank">
+      <a
+        rel="noopener"
+        href="https://www.facebook.com/jc.sabenorio.2025"
+        target="_blank"
+      >
         <FacebookIcon
           sx={{
             fontSize: 30,
